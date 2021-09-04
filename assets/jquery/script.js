7a35f6c4632369730e1b62be2d2d764f027ff269
RobMarv1n <88106847+RobMarv1n@users.noreply.github.com>
2021-09-04 12:43:41 +0300

    Add all files

---
+++ assets/jquery/script.js
@@ -0,0 +1,14 @@
+// ! HEADER SLIDER
+
+const swiper = new Swiper('.swiper-container', {
+
+    slidesPerView: 3,
+    spaceBetween: 25,
+    grabCursor: true,
+    loop: true,
+    // autoplay: 4000,
+    // speed: 400,
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+});
+
